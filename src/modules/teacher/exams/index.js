e8827b64c409f7f261c0603f0287b7e2d1d6a0f4
neo091 <mneo666@gmail.com>
2024-10-09 13:04:55 +0200

    add get exams

--- src/modules/teacher/exams/index.js
+++ src/modules/teacher/exams/index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const router = express.Router()
 const responses = require('../../../red/responses')
 const security = require('../../../middlewares/security')
-const { insert } = require('../../../DB/crud')
+const { insert, select } = require('../../../DB/crud')
 const tables = require('../../../utils/tables')
 
-router.get("/", security(), (req, res) => {
-    return responses.success(req, res, { message: "success" }, 200)
+function getExams(roomID) {
+    return select(tables.EXAMS, { roomID: roomID })
+}
+router.get("/:roomID", security(), async (req, res) => {
+
+    const roomID = req.params.roomID
+
+    const exams = await getExams(roomID)
+
+    return responses.success(req, res, { message: "success", exams }, 200)
 })
 
 const saveExam = ({ exam, config, roomID }) => insert(tables.EXAMS, { exam, config, roomID })

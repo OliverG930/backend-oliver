c99044b49feef1e0e620c7c9436035c005a1434a
neo091 <mneo666@gmail.com>
2024-11-17 18:17:19 +0100

    add: teacher register

--- src/modules/clients/register.js
+++ src/modules/clients/register.js
@@ -1,43 +1,50 @@
 const express = require('express')
 const router = express.Router()
-const resposes = require('../../red/responses')
+const responses = require('../../red/responses')
 const controller = require('../../modules/clients/registerController')
 const messages = require('../../utils/messages')
 
 router.post('/', async (req, res) => {
 
-    //recibe los datos enviados por el usuario desde un formulario en tipo JSON
-    const body = req.body
+  //recibe los datos enviados por el usuario desde un formulario en tipo JSON
+  const body = req.body
 
-    const name = body.name
-    const email = body.email
-    const password = body.password
+  const name = body.name
+  const lastName = body.lastName
+  const email = body.email
+  const password = body.password
+  const rol_id = body.rol_id ?? 1
 
-    //capa de seguridad para detectar si los campos no son vacios
-    if (name === "" || !name) {
-        return resposes.error(req, res, { message: messages.err_messages.NAME_REQUIRED }, 500)
+  //capa de seguridad para detectar si los campos no son vacíos
+  if (name === "" || !name) {
+    return responses.error(req, res, { message: messages.err_messages.NAME_REQUIRED }, 500)
+  }
+
+  if (lastName === "" || !lastName) {
+    return responses.error(req, res, { message: messages.err_messages.LAST_NAME_REQUIRED }, 500)
+  }
+
+
+  if (email === "" || !email) {
+    return responses.error(req, res, { message: messages.err_messages.EMAIL_REQUIRED }, 500)
+  }
+
+  if (password === "" || !password) {
+    return responses.error(req, res, { message: messages.err_messages.PASSWORD_REQUIRED }, 500)
+  }
+
+  try {
+    //controlador se encarga de hacer la consulta o insert de los datos enviados por el usuario
+    const data = await controller.register({ name, lastName, rol_id, email, password })
+
+    //si la data recibe un error 
+    if (data.error) {
+      return responses.error(req, res, data.message, 500)
     }
-
-    if (email === "" || !email) {
-        return resposes.error(req, res, { message: messages.err_messages.EMAIL_REQUIRED }, 500)
-    }
-
-    if (password === "" || !password) {
-        return resposes.error(req, res, { message: messages.err_messages.PASSWORD_REQUIRED }, 500)
-    }
-
-    try {
-        //controlador se encarga de hacer la consulta o insert de los datos enviados por el usuario
-        const data = await controller.register({ name, email, password })
-
-        //si la data recibe un error 
-        if (data.error) {
-            return resposes.error(req, res, data.message, 500)
-        }
-        resposes.success(req, res, data, 200)
-    } catch (err) {
-        resposes.error(req, res, err, 500)
-    }
+    responses.success(req, res, data, 200)
+  } catch (err) {
+    responses.error(req, res, err, 500)
+  }
 
 })
 
--- src/modules/clients/registerController.js
+++ src/modules/clients/registerController.js
@@ -7,32 +7,33 @@ const config = require('../../config')
 const TABLE = 'usuarios'
 const SALT_ROUND = config.jwt.salt || 10
 
-const register = async ({ name, email, password }) => {
+const register = async ({ name, lastName, rol_id, email, password }) => {
 
-    //verificamos si en la db no existe ese correo
-    const verifyUser = await db.selectOneWhere(TABLE, { correo: email })
+  //verificamos si en la db no existe ese correo
+  const verifyUser = await db.selectOneWhere(TABLE, { correo: email })
 
-    //si el usuario existe o no es nulo
-    if (verifyUser || verifyUser != null) {
-        return { error: true, message: message.err_messages.ALREADY_TAKEN }
-    }
+  //si el usuario existe o no es nulo
+  if (verifyUser || verifyUser != null) {
+    return { error: true, message: message.err_messages.ALREADY_TAKEN }
+  }
 
-    //generando la contrasenia cifrada
-    var newPasswordHash = await bcrypt.hash(password, parseInt(SALT_ROUND))
+  //generando la contrasenia cifrada
+  var newPasswordHash = await bcrypt.hash(password, parseInt(SALT_ROUND))
 
-    //con los datos recibidos generamos el nuevo usuario
-    const user = {
-        nombre: name,
-        correo: email,
-        contrasenia: newPasswordHash, //genera la contrasenia cifrada
-        rol_id: 1 //usuario por defecto
-    }
+  //con los datos recibidos generamos el nuevo usuario
+  const user = {
+    nombre: name,
+    apellido: lastName,
+    correo: email,
+    contrasenia: newPasswordHash, //genera la contrasenia cifrada
+    rol_id: rol_id ?? 1 //usuario por defecto
+  }
 
-    //procede a registrar o insertar en la DB
-    const registered = await db.insert(TABLE, user)
-    return registered
+  //procede a registrar o insertar en la DB
+  const registered = await db.insert(TABLE, user)
+  return registered
 }
 
 module.exports = {
-    register
+  register
 }
